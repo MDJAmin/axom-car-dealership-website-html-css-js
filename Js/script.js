165a2619cfea5cd04e66f99602da2a2af9ff2b3c
MDJAmin <mohammadamindf@gmail.com>
2024-06-04 20:46:31 +0430

    main page style & animation update
    i just set some new animation and also fixed some style...

--- Js/script.js
+++ Js/script.js
@@ -55,6 +55,48 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenEl = document.querySelectorAll(".hidden");
 hiddenEl.forEach((el) => observer.observe(el));
 
+
+
+const showingP = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showP");
+    } 
+  });
+});
+const hiddenP = document.querySelectorAll(".hiddenP");
+hiddenP.forEach((el) => showingP.observe(el));
+console.log(showingP)
+
+
+const showCardsone = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showCards");
+    } 
+  });
+});
+const hiddeCardsOne = document.querySelectorAll(".hiddenCardsOne",);
+const hiddenCardsTwo = document.querySelectorAll(".hiddenCardsTwo",);
+hiddeCardsOne.forEach((el) => showCardsone.observe(el));
+hiddenCardsTwo.forEach((el) => showCardsone.observe(el));
+
+const showup = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("up-show");
+    } 
+        else {
+      entry.target.classList.remove("up-show");
+    }
+  });
+});
+const hideup = document.querySelectorAll(".up-hidden");
+hideup.forEach((el) => showup.observe(el));
+
 // const itemsEl=document.querySelector('.section-3-product-div')
 
 // const scrollFunc=()=>{
