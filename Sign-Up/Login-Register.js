a0fedb804fb11825e2fd0bb68b2d60505af54ceb
MDJAmin <mohammadamindf@gmail.com>
2024-06-08 05:36:20 +0430

    Login page update
    i just add a show and hide btn for my password inp and also complete the style and theme for the login page

--- Sign-Up/Login-Register.js
+++ Sign-Up/Login-Register.js
@@ -1,2 +1,18 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById('passwordInput');
+    const togglePassword = document.getElementById('togglePassword');
+    const togglePasswordIcon = togglePassword.querySelector('i');
+
+    togglePassword.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        
+        // Toggle the eye icon
+        togglePasswordIcon.classList.toggle('bx-show');
+        togglePasswordIcon.classList.toggle('bx-hide');
+    });
+});
+
+
