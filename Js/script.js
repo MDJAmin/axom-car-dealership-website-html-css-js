264a8a2231c3ae992ab2a35ecc35db6008a06c0f
MDJAmin <mohammadamindf@gmail.com>
2024-06-09 14:17:13 +0430

    Delete the comments in html , css , js

--- Js/script.js
+++ Js/script.js
@@ -50,78 +50,6 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     // console.log(entry)
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     }
-//     // else {
-//     //   entry.target.classList.remove("show");
-//     // }
-//   });
-// });
-// const hiddenEl = document.querySelectorAll(".hidden");
-// hiddenEl.forEach((el) => observer.observe(el));
-
-// const showingP = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     // console.log(entry)
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("showP");
-//     }
-//   });
-// });
-// const hiddenP = document.querySelectorAll(".hiddenP");
-// hiddenP.forEach((el) => showingP.observe(el));
-// console.log(showingP)
-
-// const showCardsone = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     // console.log(entry)
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("showCards");
-//     }
-//   });
-// });
-// const hiddeCardsOne = document.querySelectorAll(".hiddenCardsOne",);
-// const hiddenCardsTwo = document.querySelectorAll(".hiddenCardsTwo",);
-// hiddeCardsOne.forEach((el) => showCardsone.observe(el));
-// hiddenCardsTwo.forEach((el) => showCardsone.observe(el));
-
-// const showup = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     // console.log(entry)
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("up-show");
-//     }
-//         else {
-//       entry.target.classList.remove("up-show");
-//     }
-//   });
-// });
-// const hideup = document.querySelectorAll(".up-hidden");
-// hideup.forEach((el) => showup.observe(el));
-
-// const itemsEl=document.querySelector('.section-3-product-div')
-
-// const scrollFunc=()=>{
-//     const triggerBottom=window.innerHeight * .8
-//     for(let i=0;i<itemsEl.length;i++){
-//         const top=itemsEl[i].getBoundingClientRect().top
-//         if(top<triggerBottom){
-//             itemsEl[i].classList.add('show')
-//         }else{
-//             itemsEl[i].classList.remove('show')
-
-//         }
-//     }
-// }
-// window.addEventListener('scroll',scrollFunc)
-
-// Show & hide Card and items animations
-
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -157,20 +85,6 @@ const showup = new IntersectionObserver((entries) => {
 const hideup = document.querySelectorAll(".up-hidden");
 hideup.forEach((el) => showup.observe(el));
 
-// let langBtn = document.querySelector(".lang");
-// let langMenu = document.querySelector(".lang-menu");
-
-// langBtn.addEventListener("click", () => {
-//   langMenu.style.opacity = 1;
-//   langMenu.style.display = "block";
-// });
-// langBtn.addEventListener("click", () => {
-//   langMenu.style.opacity = 0;
-//   langMenu.style.display = "none";
-// });
-
-// lang menu
-
 let langBtn = document.querySelector(".lang");
 let langMenu = document.querySelector(".lang-menu");
 
@@ -184,4 +98,3 @@ langBtn.addEventListener("click", () => {
   }
 });
 
-// localStorage.clear()
